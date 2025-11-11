7b420b401f07a542e99bfaedf52d610f1784f2e2
Xiaoze Yang <yangxiaoze@gmail.com>
2025-11-11 14:41:55 -0800

    update version 2

--- hello_app/app/main.py
+++ hello_app/app/main.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def msg():
     return "Hello World! Deployed using Argo CD!"
 
-if __name__ == "__main__"
+@app.route("/argocd")
+def argocd_msg():
+    return "Isn't Argo CD pretty amazing?"
+
+if __name__ == "__main__":
     app.run(host='0.0.0.0')
